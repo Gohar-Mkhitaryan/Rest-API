a23695b5cc020acec07c6114a86ba9b69221f487
Mkhitaryan_Gohar <103889291+Gohar-Mkhitaryan@users.noreply.github.com>
2022-06-18 08:42:16 +0300

    Create users.js

---
+++ users/users.js
@@ -0,0 +1,26 @@
+let users = [
+    {
+        id: 1,
+        name: 'Aram',
+        surname: 'Aramyan',
+        age: 25,
+        isPublic: false
+    },
+    {
+        id: 2,
+        name: 'Anna',
+        surname: 'Asatryan',
+        age: 35,
+        isPublic: true
+    },
+    {
+        id: 3,
+        name: 'Sargis',
+        surname: 'Sargsyan',
+        age: 20,
+        isPublic: false
+    }
+    ]
+    let id = users.length;
+   
+    module.exports = {users}
